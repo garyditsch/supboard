b6eb5f7b7960c1c48c7df6433c96c8ba525ef190
Gary Ditsch <garyditsch@gmail.com>
2016-10-12 14:35:05 -0400

    start sups module

---
+++ client/sups/sups.module.js
@@ -0,0 +1,8 @@
+import angular from 'angular';
+import supsPagecomonent from './sups-page.component';
+
+const SupsModule = angular.module('sups', [
+    ])
+    .component('supsPage', supsPagecomonent);
+
+export default SupsModule;
